d7a8133e1dbdaf28d5ba525533c699b15818c44f
saagrawal04 <97708779+saagrawal04@users.noreply.github.com>
2023-12-01 13:45:43 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -21,30 +21,29 @@ class ListExamples {
 
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
-  static List<String> merge(List<String> list1, List<String> list2) {
+static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
-    while(index1 < list1.size() && index2 < list2.size()) {
-      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
+    while (index1 < list1.size() && index2 < list2.size()) {
+        if (list1.get(index1).compareTo(list2.get(index2)) <= 0) {
+            result.add(list1.get(index1));
+            index1 += 1;
+        } else {
+            result.add(list2.get(index2));
+            index2 += 1;
+        }
+    }
+    while (index1 < list1.size()) {
         result.add(list1.get(index1));
         index1 += 1;
-      }
-      else {
-        result.add(list2.get(index2));
+    }
+    while (index2 < list2.size()) {
+        // Introduce a bug: Skip adding equal strings to the result list
+        if (index2 == 0 || !list2.get(index2).equals(list2.get(index2 - 1))) {
+            result.add(list2.get(index2));
+        }
         index2 += 1;
-      }
-    }
-    while(index1 < list1.size()) {
-      result.add(list1.get(index1));
-      index1 += 1;
-    }
-    while(index2 < list2.size()) {
-      result.add(list2.get(index2));
-      // change index1 below to index2 to fix test
-      index1 += 1;
     }
     return result;
-  }
-
-
 }
+
